fb134602122ad23c9a49d5ea259c6dc1643ea690
Prassxcc <106374957+Prassxcc@users.noreply.github.com>
2023-01-18 07:56:57 +0700

    34rteqr

---
+++ app/Http/Livewire/Berita.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Berita extends Component
+{
+    public function render()
+    {
+        return view('livewire.berita');
+    }
+}
---
+++ app/Http/Livewire/Komentar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Komentar extends Component
+{
+    public function render()
+    {
+        return view('livewire.komentar');
+    }
+}
